401d33956b51fe41f75e7e6081b8350d05ad3447
Frans-Lukas <franslukas@gmail.com>
2019-02-17 16:10:26 -0500

    first commit

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+SUFFIX="-franslukas"
+
+set -e
+
+make
+
+make modules_install
+
+cp arch/x86_64/boot/bzImage /boot/vmlinuz-linux$SUFFIX
+
+sed s/linux/linux$SUFFIX/g \
+	</etc/mkinitcpio.d/linux.preset \
+	>/etc/mkinitcpio.d/linux$SUFFIX.preset
+mkinitcpio -p linux$SUFFIX
+
+grub-mkconfig -o /boot/grub/grub.cfg
